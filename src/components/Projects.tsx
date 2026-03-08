39b8a05d97fc4b4a366102d0a43e6e0b1eeadbe1
rchll-16 <pantinoplerachelle16@gmail.com>
2026-03-08 19:50:43 +0800

    Added a new two projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,77 +11,103 @@ interface Project {
 }
 
 const projects: Project[] = [
-  {
-    title: 'Molave Street Barbers (Capstone Project)',
-    description: 'A modern, responsive website built as a capstone project for Molave Street Barbers. The site features a clean design, service showcases, and intuitive navigation—focused on delivering a polished user experience across desktop and mobile devices.',
-    tech: ['React', 'JavaScript', 'Tailwind', 'Supabase'],
-    liveUrl: 'https://molavestreetbarbers.vercel.app/',
-    githubUrl: 'https://github.com/rchll-16/molavestreetbarbers-.git',
-  },
-  {
-    title: 'Rapido IT Solutions (Commision)',
-    description: 'A professional and fully responsive corporate website developed for Rapido Solutions. Designed to clearly present the company’s services, brand identity, and contact information with a clean layout and smooth user experience across all devices.',
-    tech: ['React', 'JavaScript', 'Tailwind', 'Supabase'],
-    liveUrl: 'https://rapidosolutions.tech/',
-    githubUrl: 'https://github.com/rchll-16/rapido-it-solution.git',
-  },
-  {
-    title: 'Flower Shop',
-    description: 'The Flower Shop is a static website designed to showcase a variety of flowers available for purchase. This website provides users with an easy-to-navigate interface to explore different flower types, view details, and make inquiries.',
-    tech: ['HTML', 'CSS', 'JavaScript'],
-    liveUrl: 'https://rchll-16.github.io/Flower-Shop/',
-    githubUrl: 'https://github.com/rchll-16/Flower-Shop.git',
-  },
-  {
-    title: 'E-Commerce Web App',
-    description: 'An E-Commerce bookstore web application where users can browse, search, and purchase books online. Built with React, Node.js, and MongoDB — providing a smooth, and dynamic shopping experience.',
-    tech: ['React', 'JavaScript', 'Node.js', 'MongoDB'],
-    liveUrl: '#',
-    githubUrl: 'https://github.com/rchll-16/e-commerce.git',
-  },
-  {
-    title: 'Chill N Grind (Commission)',
-    description: 'A clean user-friendly clothing site built with HTML and CSS. This static web project showcases a landing page, FAQ section, and styling using custom CSS.',
-    tech: ['HTML', 'CSS'],
-    liveUrl: 'https://rchll-16.github.io/clothingwebsite/main.html',
-    githubUrl: 'https://github.com/rchll-16/clothingwebsite.git',
-  },
-  {
-    title: 'Alpha Angelicum Academy (Commission)',
-    description: 'A clean and informative school website crafted with HTML and CSS. Featuring multiple pages—Home, About, Academic Programs, Offers, Contacts.',
-    tech: ['HTML', 'CSS'],
-    liveUrl: 'https://rchll-16.github.io/school-aaa/',
-    githubUrl: 'https://github.com/rchll-16/school-aaa.git',
-  },
-  {
-    title: 'Univerity of Eastern Pangasinan (Commission)',
-    description: 'A clean and informative school website built using HTML and CSS. Ideal for institutional presentation, this static site includes pages for home, about, academic programs, events, and contact details.',
-    tech: ['HTML', 'CSS'],
-    liveUrl: 'https://rchll-16.github.io/school-uep/',
-    githubUrl: 'https://github.com/rchll-16/school-uep.git',
-  },
-  {
-    title: 'Intramuros Cultural (Commision)',
-    description: 'A beautifully designed, static website showcasing the heritage sites of Intramuros using HTML, CSS, and high-quality images. Perfect for both educational presentation and visual storytelling of historical landmarks.',
-    tech: ['HTML', 'CSS'],
-    liveUrl: 'https://rchll-16.github.io/intramuros-website/',
-    githubUrl: 'https://github.com/rchll-16/intramuros-website.git',
-  },
-  {
-    title: 'Science Module (Commission)',
-    description: 'A clean, educational website designed to present various science topics using HTML and CSS. Perfect for sharing lesson modules such as the water cycle, biodiversity, pollution, and more in a readable, structured format.',
-    tech: ['HTML', 'CSS'],
-    liveUrl: 'https://rchll-16.github.io/science-lesson/main.html',
-    githubUrl: 'https://github.com/rchll-16/science-lesson.git',
-  },
-  {
-    title: 'Bini (Redesign)',
-    description: 'A clean, modern redesign project for BINI, focusing on visually appealing layout, and smooth user experience. Built entirely with HTML, CSS, and JavaScript — no frameworks, just handcrafted code.',
-    tech: ['HTML', 'CSS', 'JavaScript'],
-    liveUrl: 'https://rchll-16.github.io/bini-redesign/main.html',
-    githubUrl: 'https://github.com/rchll-16/bini-redesign.git',
-  }
-];
+	{
+		title: "CodeWave",
+		description:
+			"A modern and responsive business landing page built with React, TypeScript, and Tailwind CSS. The project highlights clean UI design, smooth animations, and scalable component architecture. It includes sections such as hero, portfolio, testimonials, team, and contact, designed to create a professional and engaging user experience.",
+		tech: ["React", "TypeScript", "Tailwind", "Vite"],
+		liveUrl: "https://codewave-flame.vercel.app/",
+		githubUrl: "https://github.com/rchll-16/codewave-digital-agency.git",
+	},
+	{
+		title: "My First Portfolio Website",
+		description:
+			"Personal portfolio website showcasing my projects, skills, and development experience. Built with modern web technologies and responsive design to present my work in a clean and professional interface.",
+		tech: ["React", "JavaScript", "Tailwind", "Vite"],
+		liveUrl: "https://rachelle-pantinople.vercel.app/",
+		githubUrl: "https://github.com/rchll-16/my-portfolio.git",
+	},
+	{
+		title: "Molave Street Barbers (Capstone Project)",
+		description:
+			"A modern, responsive website built as a capstone project for Molave Street Barbers. The site features a clean design, service showcases, and intuitive navigation—focused on delivering a polished user experience across desktop and mobile devices.",
+		tech: ["React", "JavaScript", "Tailwind", "Supabase", "Vite"],
+		liveUrl: "https://molavestreetbarbers.vercel.app/",
+		githubUrl: "https://github.com/rchll-16/molavestreetbarbers-.git",
+	},
+	{
+		title: "Rapido IT Solutions (Commision)",
+		description:
+			"A professional and fully responsive corporate website developed for Rapido Solutions. Designed to clearly present the company’s services, brand identity, and contact information with a clean layout and smooth user experience across all devices.",
+		tech: ["React", "JavaScript", "Tailwind", "Supabase", "Vite"],
+		liveUrl: "https://rapidosolutions.tech/",
+		githubUrl: "https://github.com/rchll-16/rapido-it-solution.git",
+	},
+	{
+		title: "Flower Shop",
+		description:
+			"The Flower Shop is a static website designed to showcase a variety of flowers available for purchase. This website provides users with an easy-to-navigate interface to explore different flower types, view details, and make inquiries.",
+		tech: ["HTML", "CSS", "JavaScript"],
+		liveUrl: "https://rchll-16.github.io/Flower-Shop/",
+		githubUrl: "https://github.com/rchll-16/Flower-Shop.git",
+	},
+	{
+		title: "E-Commerce Web App",
+		description:
+			"An E-Commerce bookstore web application where users can browse, search, and purchase books online. Built with React, Node.js, and MongoDB — providing a smooth, and dynamic shopping experience.",
+		tech: ["React", "JavaScript", "Node.js", "MongoDB"],
+		liveUrl: "#",
+		githubUrl: "https://github.com/rchll-16/e-commerce.git",
+	},
+	{
+		title: "Chill N Grind (Commission)",
+		description:
+			"A clean user-friendly clothing site built with HTML and CSS. This static web project showcases a landing page, FAQ section, and styling using custom CSS.",
+		tech: ["HTML", "CSS"],
+		liveUrl: "https://rchll-16.github.io/clothingwebsite/main.html",
+		githubUrl: "https://github.com/rchll-16/clothingwebsite.git",
+	},
+	{
+		title: "Alpha Angelicum Academy (Commission)",
+		description:
+			"A clean and informative school website crafted with HTML and CSS. Featuring multiple pages—Home, About, Academic Programs, Offers, Contacts.",
+		tech: ["HTML", "CSS"],
+		liveUrl: "https://rchll-16.github.io/school-aaa/",
+		githubUrl: "https://github.com/rchll-16/school-aaa.git",
+	},
+	{
+		title: "Univerity of Eastern Pangasinan (Commission)",
+		description:
+			"A clean and informative school website built using HTML and CSS. Ideal for institutional presentation, this static site includes pages for home, about, academic programs, events, and contact details.",
+		tech: ["HTML", "CSS"],
+		liveUrl: "https://rchll-16.github.io/school-uep/",
+		githubUrl: "https://github.com/rchll-16/school-uep.git",
+	},
+	{
+		title: "Intramuros Cultural (Commision)",
+		description:
+			"A beautifully designed, static website showcasing the heritage sites of Intramuros using HTML, CSS, and high-quality images. Perfect for both educational presentation and visual storytelling of historical landmarks.",
+		tech: ["HTML", "CSS"],
+		liveUrl: "https://rchll-16.github.io/intramuros-website/",
+		githubUrl: "https://github.com/rchll-16/intramuros-website.git",
+	},
+	{
+		title: "Science Module (Commission)",
+		description:
+			"A clean, educational website designed to present various science topics using HTML and CSS. Perfect for sharing lesson modules such as the water cycle, biodiversity, pollution, and more in a readable, structured format.",
+		tech: ["HTML", "CSS"],
+		liveUrl: "https://rchll-16.github.io/science-lesson/main.html",
+		githubUrl: "https://github.com/rchll-16/science-lesson.git",
+	},
+	{
+		title: "Bini (Redesign)",
+		description:
+			"A clean, modern redesign project for BINI, focusing on visually appealing layout, and smooth user experience. Built entirely with HTML, CSS, and JavaScript — no frameworks, just handcrafted code.",
+		tech: ["HTML", "CSS", "JavaScript"],
+		liveUrl: "https://rchll-16.github.io/bini-redesign/main.html",
+		githubUrl: "https://github.com/rchll-16/bini-redesign.git",
+	},
+]
 
 
 const Projects = () => {
